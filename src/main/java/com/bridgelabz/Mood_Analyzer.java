e6539ebcd6a17dfde5dc6a3d1f1330cc076a4e98
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-22 13:24:13 +0530

    Added : analyseMood method

--- src/main/java/com/bridgelabz/Mood_Analyzer.java
+++ src/main/java/com/bridgelabz/Mood_Analyzer.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
-
+// UC 1
+// Given a Message, ability to analyse and respond Happy or Sad Mood
+// - Create MoodAnalyser Object
+// - Call analyseMood function with message as parameter and return Happy or Sad Mood
 public class Mood_Analyzer {
     public static void main(String[] args) {
         System.out.println("Mood Analyzer Problem");
+    }
+    public String analyseMood(String msg){
+        if(msg.contains("sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
     }
 }
