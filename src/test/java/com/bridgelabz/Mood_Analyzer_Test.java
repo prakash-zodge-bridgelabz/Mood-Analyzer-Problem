2140f58b3af03184c2e2ef498197319b7f7a5529
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-22 14:15:04 +0530

    Repeat Test Case : 1.2 : Given msg in constructor should return HAPPY

--- src/test/java/com/bridgelabz/Mood_Analyzer_Test.java
+++ src/test/java/com/bridgelabz/Mood_Analyzer_Test.java
@@ -4,17 +4,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-// Repeat Test Case : 1.1
-// Given “I am in Sad Mood” message in Constructor Should Return SAD
-// - To pass this Test Case when calling analyseMood method with no params should return SAD
+// Repeat Test Case : 1.2
+// Given “I am in Happy Mood” message in Constructor Should Return SAD
+// - To pass this Test Case when calling analyseMood method with no params should return HAPPY
 public class Mood_Analyzer_Test {
     Mood_Analyzer mood;
 
     @Test
-    public void test_analyseMood_sad(){
-        mood = new Mood_Analyzer("I am in sad Mood");
+    public void test_analyseMood(){
+        mood = new Mood_Analyzer("I am in Happy Mood");
         String actual = mood.analyseMood();
-        Assert.assertEquals("SAD",actual);
+        Assert.assertEquals("HAPPY",actual);
     }
 
 }
