e2c098a25912eea88c776f3ef1bb21a51e9ee281
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-24 15:00:32 +0530

    Modified: Test Case 3.2: Given empty mood should throw MoodAnalysisException indicating Empty Mood

--- src/test/java/com/bridgelabz/Mood_Analyzer_Test.java
+++ src/test/java/com/bridgelabz/Mood_Analyzer_Test.java
@@ -3,17 +3,18 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-// Test Case 3.1
-// Given NULL Mood Should Throw MoodAnalysisException
-// -To pass this Test Case in try catch block throw MoodAnalysisException
+// Test Case 3.2
+// Given Empty Mood Should Throw MoodAnalysisException indicating Empty Mood
+// Handle Empty Mood Scenario throw MoodAnalysisException and inform user of the EmptyMood
+// HINT: Use Enum to EMPTY or NULL
 public class Mood_Analyzer_Test {
     Mood_Analyzer mood;
 
     @Test
     public void test_analyseMood(){
-        mood = new Mood_Analyzer(null);
+        mood = new Mood_Analyzer("");
         String actual = mood.analyseMood();
-        Assert.assertEquals("Custom Exception: Null mood",actual);
+        Assert.assertEquals("Custom Exception: Empty mood",actual);
     }
 
 }
